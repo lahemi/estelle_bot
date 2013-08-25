7f817e751941827bb212c302730b2e66ff13a51d
lahemi <lahemi.maki@gmail.com>
2013-08-25 06:11:26 +0300

    !help+awk function docs

--- estelle_bot.lua
+++ estelle_bot.lua
@@ -6,10 +6,13 @@
 -- Our "environment".
 local socket = require("socket")
 local http   = require("socket.http")
-local string = { sub   = string.sub,
-                 gsub  = string.gsub,
-                 find  = string.find,
-                 match = string.match }
+local table  = { concat = table.concat }
+local string = { sub    = string.sub,
+                 gsub   = string.gsub,
+                 find   = string.find,
+                 lower  = string.lower,
+                 match  = string.match,
+                 gmatch = string.gmatch }
 local print = print
 
 -- Configuration. Fill in as your heart desires.
@@ -34,8 +37,47 @@ local msg = function(s, channel, content)
     print(content)
 end
 
+-- A little trick for creating strings. var="stuff\ more stuff" C-stylish
+-- breaking of long strings doesn't work in Lua. One option would've been
+-- to use concatenation (..). That, however, is really inefficient, since
+-- each concatenation call creates a new string and allocates its memory
+-- and some such. This table.concat{} method is a nice way around it.
+local awkhelp = table.concat{"man awk; ","man gawk; ",
+                             "http://awk.freeshell.org/; ","http://awk.info; ",
+                             "http://www.gnu.org/software/gawk/manual/"}
+-- We have our custom "manpage", with shorter entries and less dribble.
+local awkdocs = "estelledocs_awk_funcs.txt"
+local awkpicker = function(name)
+    local name = string.lower(name)
+    local fh = io.open(awkdocs)
+    local rd = fh:read('*a')
+    fh:close()
+    for line in rd:gmatch("[^\n]+") do
+        if line:match("^"..name) then
+            return line
+        end
+    end
+end
+
+local bashhelp = table.concat{"man bash; ",
+                              "http://wiki.bash-hackers.org/start; ",
+                              "http://mywiki.wooledge.org/BashGuide"}
+
 local process = function(s, channel, lnick, line)
     if line:find("^!exit") and lnick == overlord then os.exit() end
+    if line:find("^!help") then
+        if line:match("^!help awk$") then
+            msg(s, channel, lnick .. ': ' .. awkhelp)
+        elseif line:match("^!help awk.+$") then
+            local pat = line:gsub("^!help awk ","")
+            local helpfun = awkpicker(pat)
+            msg(s, channel, lnick .. ': ' .. helpfun)
+        elseif line:match("^!help bash$") then
+            msg(s, channel, lnick .. ': ' .. bashhelp)
+        else
+            msg(s, channel, lnick .. ': Have you tried to RTFM? :)')
+        end
+    end
     if line:find("^https?://") then
         local page = ""
         if line:find("%s") then
